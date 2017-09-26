e7f522576a8e9ae3fc327372286eee98dedaef87
Dan Fusaro <dan@industrialcolor.com>
2017-09-26 09:34:33 -0400

    Added comments to config

--- app/js/config.js
+++ app/js/config.js
@@ -1,13 +1,20 @@
 export const config = {
+  // Game board width
   width: 1200,
+  // Game board height
   height: 800,
+  // Size of a sprite, square
   size: 32,
+  // Game colors
   colors: {
     snake: '#00FF00',
     food: 'white',
     board: ['black', 'blue']
   },
+  // Initial speed
   speed: 2,
+  // How many points until level up
   levelUp: 3,
+  // Default screen font
   font: 'Arial'
 };
